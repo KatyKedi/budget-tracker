187b2e6bfeef40b76dcd68c9d09827fcc03fc57f
KatyKedi <katykat5bros@gmail.com>
2022-05-04 13:25:56 -0700

    indexDB for offline functionality

---
+++ public/js/idb.js
@@ -0,0 +1,73 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+// upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+  
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new deposit or expense and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const objectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    objectStore.add(record);
+}
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const objectStore = transaction.objectStore('new_transaction');
+    const getAll = objectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                // open one more transaction
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                // access the new_pizza object store
+                const objectStore = transaction.objectStore('new_transaction');
+                // clear all items in your store
+                objectStore.clear();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+}
+
+window.addEventListener('online', uploadTransaction);
